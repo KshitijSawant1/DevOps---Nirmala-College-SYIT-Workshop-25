ff93b73f63838427f3f7c211c473167af22ab27e
KSHITIJ K SAWANT <sawantkshitij1@gmail.com>
2025-09-13 16:10:15 +0530

    Pre Session Day 2 - 4

--- Session 2/Python Flask App/test_app.py
+++
@@ -1,7 +0,0 @@
-from app import app
-
-def test_home():
-    c = app.test_client()
-    r = c.get("/")
-    assert r.status_code == 200
-    assert "Hello from Flask" in r.get_data(as_text=True)
--- Session 2/app.py
+++ Session 2/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Flask + Jenkins (no Docker)!"
+    return "Hello, DevOps Workshop with Flask!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
