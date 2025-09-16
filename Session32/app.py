6d96345e8a68f24229585748059a175e93fd841f
KSHITIJ K SAWANT <sawantkshitij1@gmail.com>
2025-09-16 10:48:11 +0530

    Session 3 Post

--- Session32/app.py
+++ Session32/app.py
@@ -12,4 +12,4 @@ def health():
 
 if __name__ == "__main__":
     # 0.0.0.0 so Jenkins/other machines can reach it; use 5001 to avoid clashes
-    app.run(host="0.0.0.0", port=5001)
+    app.run(host="0.0.0.0", port=5002)
